dfa3429864d5c1f5eb211ea27453f0498eb8635b
TalalBnoob <79761207+TalalBalnoob@users.noreply.github.com>
2025-08-02 14:37:09 +0300

    fix cors error for client app

--- Stockly/Program.cs
+++ Stockly/Program.cs
@@ -5,6 +5,17 @@ using Stockly.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("defaultString");
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -26,6 +37,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use CORS
+app.UseCors("AllowReactApp");
 
 app.UseMiddleware<ErrorHanlingMiddleware>();
 app.MapControllers();
