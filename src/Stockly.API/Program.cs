da8c788a7d37f39a4ca03060b1042c9f5e8a9fc4
TalalBalnoob <TalalBalnoob@gmail.com>
2026-01-19 14:38:48 +0300

    init

---
+++ src/Stockly.API/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Stockly.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// db config 
+builder.Services.AddDbContext<AppDbContext>(options => {
+	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+	app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
+	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ tests/Stockly.Application.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Stockly.Application.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
