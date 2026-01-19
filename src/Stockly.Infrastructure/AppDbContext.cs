f37da70aa3665f185826ee0f7d4602848f1a3a84
TalalBalnoob <TalalBalnoob@gmail.com>
2026-01-19 15:02:39 +0300

    feat: add all entitis to the db context

--- src/Stockly.Infrastructure/AppDbContext.cs
+++ src/Stockly.Infrastructure/AppDbContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Stockly.Domain.Entity;
 
 namespace Stockly.Infrastructure;
 
 public class AppDbContext : DbContext {
 	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
 	}
+
+	DbSet<Product> Products { get; set; }
+	DbSet<Stock> Stocks { get; set; }
+	DbSet<Order> Orders { get; set; }
+	DbSet<OrderItem> OrderItems { get; set; }
+	DbSet<StockAdjustment> StockAdjustments { get; set; }
 }
