99ee2fa0ec8a630094fddbaa590a40d519e3beaf
TalalBalnoob <TalalBalnoob@gmail.com>
2026-01-23 15:24:16 +0300

    feat: extract IStockService interface and create OrderItemsService class

---
+++ src/Stockly.Application/Services/OrderItemsService.cs
@@ -0,0 +1,6 @@
+using Stockly.Application.Interfaces.IRepository;
+
+namespace Stockly.Application.Services;
+
+public class OrderItemsService(IOrderItemRepository orderItemRepo) {
+}
--- src/Stockly.Application/Services/StockService.cs
+++ src/Stockly.Application/Services/StockService.cs
@@ -5,15 +5,6 @@ using Stockly.Domain.Entity;
 
 namespace Stockly.Application.Services;
 
-public interface IStockService {
-	Task<IEnumerable<Stock>> GetAllStocks();
-	Task<Stock?> GetStockById(Guid id);
-	Task<Stock?> GetStockByProductId(Guid productId);
-	Task<Stock> AddNewStock(NewStockDto stock);
-	Task<Stock> UpdateStock(UpdateStockDto stockDto);
-	void DeleteStock(Guid id);
-}
-
 public class StockService(IStockRepository stockRepo, IProductRepository productRepo, IProductService productService)
 	: IStockService {
 	public async Task<IEnumerable<Stock>> GetAllStocks() {
