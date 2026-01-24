f1a15ea3be1b43fcb11ed968d0efca45af3af9ad
TalalBalnoob <TalalBalnoob@gmail.com>
2026-01-24 16:57:56 +0300

    refactor: add repository dependencies to OrderService constructor and adjust DTO locations
    - Inject `IOrderRepository` and `IOrderItemRepository` into `OrderService`.
    - Move `NewCreatedProductDto` and `NewProductDto` to `CreateProductWithStock` use case folder.

--- src/Stockly.Application/Services/OrderService.cs
+++ src/Stockly.Application/Services/OrderService.cs
@@ -1,6 +1,8 @@
+using Stockly.Application.Interfaces.IRepository;
 using Stockly.Application.Interfaces.Services;
+using Stockly.Domain.Entity;
 
 namespace Stockly.Application.Services;
 
-public class OrderService : IOrderService {
+public class OrderService(IOrderRepository orderRepo, IOrderItemRepository orderItemRepo) : IOrderService {
 }
