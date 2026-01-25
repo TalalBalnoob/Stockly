9c96e9bfee8566b8f96d919dd19c35418845e560
TalalBalnoob <TalalBalnoob@gmail.com>
2026-01-25 14:55:13 +0300

    feat: add `NewOrderDto` to define structure for new order creation
    - Includes properties for order status, total price, customer details, payment status, and order items.

---
+++ src/Stockly.Application/DTOs/NewOrderDto.cs
@@ -0,0 +1,16 @@
+using Stockly.Domain.Enums;
+
+namespace Stockly.Application.DTOs;
+
+public class NewOrderDto {
+	public OrderStatus Status { get; set; } = OrderStatus.Processing;
+	public decimal TotalPrice { get; set; }
+	public string? Note { get; set; }
+	public string? CustomerName { get; set; }
+	public string? CustomerContact { get; set; }
+	public string? CustomerAddress { get; set; }
+	public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Unpaid;
+	public string? PaymentDetails { get; set; }
+
+	public NewOrderItemDto[] OrderItems { get; set; } = Array.Empty<NewOrderItemDto>();
+}
