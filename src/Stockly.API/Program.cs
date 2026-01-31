be8b186e5f9c851c35fcef460304d3727b988bca
TalalBalnoob <TalalBalnoob@gmail.com>
2026-01-31 15:01:01 +0300

    feat: configure dependency injection, add Swagger, and set up controllers
    - Replaced `Singleton` services with `Scoped` for repositories and services.
    - Registered new use cases in the service container.
    - Added Swagger and API explorer for documentation.
    - Enabled controller mapping for API endpoints.

--- src/Stockly.API/Program.cs
+++ src/Stockly.API/Program.cs
@@ -1,12 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Stockly.Application.Interfaces.IRepository;
 using Stockly.Application.Interfaces.Services;
+using Stockly.Application.Interfaces.UseCases;
 using Stockly.Application.Services;
+using Stockly.Application.UseCases.CreateNewOrder;
+using Stockly.Application.UseCases.CreateProductWithStock;
+using Stockly.Application.UseCases.DeleteOrder;
+using Stockly.Application.UseCases.DeleteProductAndStock;
+using Stockly.Application.UseCases.UpdateOrder;
 using Stockly.Infrastructure;
 using Stockly.Infrastructure.Repositories;
 using IStockService = Stockly.Application.Interfaces.Services.IStockService;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -15,16 +26,24 @@ builder.Services.AddOpenApi();
 // db config 
 builder.Services.AddDbContext<AppDbContext>(options => { options.UseSqlite("Data Source=stockly.db"); });
 
-builder.Services.AddSingleton<IProductRepository, ProductRepository>();
-builder.Services.AddSingleton<IOrderRepository, OrderRepository>();
-builder.Services.AddSingleton<IOrderItemRepository, OrderItemRepository>();
-builder.Services.AddSingleton<IStockRepository, StockRepository>();
-builder.Services.AddSingleton<IStockAdjustmentRepository, StockAdjustmentRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<IStockAdjustmentRepository, StockAdjustmentRepository>();
 
-builder.Services.AddSingleton<IProductService, ProductService>();
-builder.Services.AddSingleton<IStockService, StockService>();
-builder.Services.AddSingleton<IOrderItemsService, OrderItemsService>();
-builder.Services.AddSingleton<IOrderService, OrderService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IStockService, StockService>();
+builder.Services.AddScoped<IOrderItemsService, OrderItemsService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IStockAdjustmentService, StockAdjustmentService>();
+
+builder.Services.AddScoped<ICreateNewOrderUseCase, CreateNewOrderUseCase>();
+builder.Services.AddScoped<ICreateProductWithStockUseCase, CreateProductWithStockUseCase>();
+builder.Services.AddScoped<IDeleteOrderUseCase, DeleteOrderUseCase>();
+builder.Services.AddScoped<IDeleteProductAndStock, DeleteProductAndStock>();
+builder.Services.AddScoped<IUpdateOrderDtoUseCase, UpdateOrderDtoUseCase>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,6 +52,10 @@ if (app.Environment.IsDevelopment()) {
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
