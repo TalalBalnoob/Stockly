c72fe335aae0b4546aeb9d17555559e17dda28d7
TalalBalnoob <TalalBalnoob@gmail.com>
2026-01-31 12:03:17 +0300

    feat: add validations to `NewProductDto` properties
    - Added `[Range]` attribute to enforce non-negative values for `Price` and `InialQuantity`.
    - Introduced error messages for invalid values.

--- src/Stockly.Application/UseCases/CreateProductWithStock/NewProductDto.cs
+++ src/Stockly.Application/UseCases/CreateProductWithStock/NewProductDto.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Stockly.Application.DTOs;
 
 public class NewProductDto {
 	public string Name { get; set; }
 	public string? Description { get; set; } = string.Empty;
+
+	[Range(0, int.MaxValue, ErrorMessage = "Can't have negative Price")]
 	public decimal Price { get; set; }
+
 	public bool? IsActive { get; set; } = true;
+
+	[Range(0, int.MaxValue, ErrorMessage = "Can't have negative quantity")]
 	public int InialQuantity { get; set; }
 }
