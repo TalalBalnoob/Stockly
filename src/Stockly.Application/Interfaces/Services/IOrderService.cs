72694e0951c6e2578df971f4040be6326b69afb2
TalalBalnoob <TalalBalnoob@gmail.com>
2026-02-01 15:06:52 +0300

    feat: add methods to `IOrderService` for order and payment status management
    - Added `GetById`, `GetAll`, `SetOrderStatus`, and `SetPaymentStatus` methods to `IOrderService` for enhanced functionality.
    - Included necessary domain references to support the new features.

--- src/Stockly.Application/Interfaces/Services/IOrderService.cs
+++ src/Stockly.Application/Interfaces/Services/IOrderService.cs
@@ -1,5 +1,16 @@
+using Stockly.Domain.Entity;
+using Stockly.Domain.Enums;
+
 namespace Stockly.Application.Interfaces.Services;
 
-public interface IOrderService {
-	
+public interface IOrderService
+{
+    public Task<Order> GetById(Guid id);
+
+    public Task<List<Order>> GetAll();
+
+    public Task<Order> SetOrderStatus(Guid orderId, OrderStatus newStatus);
+
+    public Task<Order> SetPaymentStatus(Guid orderId, PaymentStatus newStatus);
 }
+
