03d6aacf5e5d728f76c21087f74f4515e40be208
TalalBalnoob <TalalBalnoob@gmail.com>
2026-02-02 10:39:01 +0300

    fix: correct typo in `UpdateStockDto` property name from `InialQuantity` to `Quantity`

--- src/Stockly.Application/DTOs/UpdateStockDto.cs
+++ src/Stockly.Application/DTOs/UpdateStockDto.cs
@@ -3,6 +3,6 @@ namespace Stockly.Application.DTOs;
 public class UpdateStockDto {
 	public Guid Id { get; set; }
 	public Guid ProductId { get; set; }
-	public int InialQuantity { get; set; }
+	public int Quantity { get; set; }
 	public string? StorageNote { get; set; }
 }
